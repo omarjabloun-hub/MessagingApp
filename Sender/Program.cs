7f48642898f00d0cb692657646f45473c681e260
jablo <omar.jabloun@insat.u-carthage.tn>
2023-03-01 18:40:30 +0100

    initial commit

---
+++ Sender/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+const string message = "Hello World!";
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange: "test_exchange",
+    routingKey: "red",
+    basicProperties: null,
+    body: body);
+Console.WriteLine($" [x] Sent {message}");
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
